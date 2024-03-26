6098cf65306354b4459643cbfc25b57e7bd5064f
Maxime Haselbauer <renn0xtek9@laposte.net>
2024-03-26 15:40:26 +0100

    Integrate yt_dlp in there

--- get_mp3_from_url/get_mp3_from_url/song_downloader.py
+++ get_mp3_from_url/get_mp3_from_url/song_downloader.py
@@ -1,18 +1,55 @@
 """Utility functions to download mp3 from url."""
+
+import logging
 from pathlib import Path
 
-def download_songs(batch_file_path:Path,destination_folder:Path,already_downloaded_list_file_path=Path) ->None :
+import yt_dlp
+
+logging.basicConfig(level=logging.INFO)
+
+
+def get_output_template(destination_folder: Path) -> str:
+    """Create output template of youtube-dl being given the destination folder.
+
+    Args:
+        destination_folder (Path): folder where song should be recorded
+    """
+    return str(destination_folder) + r"/%(title)s.%(ext)s"
+
+
+def download_songs(batch_file_path: Path, destination_folder: Path, already_downloaded_list_file_path=Path) -> None:
     """Download songs listed by url in batch_file_path and put them in destination folder.
 
     Args:
-        batch_file_path (Path): path to the batch file 
+        batch_file_path (Path): path to the batch file
         destination_folder (Path): path to the destination folder
-        already_downloaded_list_file_path (Path): path to the cache file 
+        already_downloaded_list_file_path (Path): path to the cache file. Will create one if none.
     """
-    for required_file in [batch_file_path,already_downloaded_list_file_path]:
-        if not required_file.exists():
-            raise FileNotFoundError(f"File not found: {required_file}")
+    if not batch_file_path.exists():
+        raise FileNotFoundError(f"File not found: {batch_file_path}")
     if not destination_folder.exists():
         raise FileNotFoundError(f"Folder not found: {destination_folder}")
+    if not already_downloaded_list_file_path.exists():
+        logging.warning("Cache file not found %s. Will create one", str(already_downloaded_list_file_path))
 
-    raise NotImplementedError
+    youtube_downloader_options = {
+        "nooverwrites": True,
+        "download_archive": str(already_downloaded_list_file_path),
+        "outtmpl": get_output_template(destination_folder),
+        "writethumbnail": True,
+        "noplaylist": True,
+        "keepvideo": True,
+        "extract_flat": True,
+        "ignoreerrors": True,  # Ignore errors during extraction
+        "skip_download": True,  # Skip downloading if extraction fails
+        "youtube_include_dash_manifest": False,  # Do not include DASH manifests (if possible)
+        "verbose": True,
+    }
+
+    # pylint: disable=(consider-using-with)
+    url_list = [line.rstrip("\n") for line in open(str(batch_file_path), encoding="utf-8")]
+
+    with yt_dlp.YoutubeDL(youtube_downloader_options) as youtube_downloader:
+        for url in url_list:
+            logging.info("Now downloading: %s", url)
+            youtube_downloader.download([url])
--- get_mp3_from_url/get_mp3_from_url/test_song_downloader.py
+++ get_mp3_from_url/get_mp3_from_url/test_song_downloader.py
@@ -2,18 +2,28 @@
 
 import unittest
 from pathlib import Path
-from .song_downloader import download_songs
+
+from .song_downloader import download_songs, get_output_template
 
 
 class TestDownloadSongs(unittest.TestCase):
     def test_download_songs_raise_if_batch_file_not_found(self):
         with self.assertRaises(FileNotFoundError):
-            download_songs(Path("Not existing"),Path("/home/foo/downloads"),Path("/home/foo/.get_mp3_from_url/cache.txt"))
-    
+            download_songs(
+                Path("Not existing"), Path("/home/foo/downloads"), Path("/home/foo/.get_mp3_from_url/cache.txt")
+            )
+
     def test_download_songs_raise_if_destination_folder_not_found(self):
         with self.assertRaises(FileNotFoundError):
-            download_songs(Path(__file__),Path("not existing"),Path("/home/foo/.get_mp3_from_url/cache.txt"))
+            download_songs(Path(__file__), Path("not existing"), Path("/home/foo/.get_mp3_from_url/cache.txt"))
 
     def test_download_songs_raise_if_cache_file_not_found(self):
         with self.assertRaises(FileNotFoundError):
-            download_songs(Path(__file__),Path(__file__).parent,Path("not existing"))
+            download_songs(Path(__file__), Path(__file__).parent, Path("not existing"))
+
+
+class TestGetOutputTemplate(unittest.TestCase):
+    def test_get_output_template(self):
+        destination_path = Path(Path.home() / "Downloads" / "mp3_from_url")
+        expected_template = str(destination_path) + "/%(title)s.%(ext)s"
+        self.assertEqual(expected_template, get_output_template(destination_path))
